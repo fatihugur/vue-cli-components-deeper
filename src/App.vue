474b4c5f14dd78b90d0150f7eb1880f87266b769
Fatih Ugur <fatihugur@gmail.com>
2021-02-01 14:53:05 +0100

    addContact function added and pushed data into App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
-    <new-friend></new-friend>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -48,6 +48,16 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
     },
   },
 };
--- src/components/NewFriend.vue
+++ src/components/NewFriend.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent="submitData">
     <div>
       <label>Name</label>
-      <input type="text" />
+      <input type="text" v-model="enteredName" />
     </div>
     <div>
       <label>Phone</label>
-      <input type="tel" />
+      <input type="tel" v-model="enteredPhone" />
     </div>
     <div>
       <label>E-mail</label>
-      <input type="e-mail" />
+      <input type="e-mail" v-model="enteredEmail" />
     </div>
     <div>
       <button>Add Contact</button>
@@ -18,5 +18,24 @@
   </form>
 </template>
 <script>
-export default {};
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
 </script>
